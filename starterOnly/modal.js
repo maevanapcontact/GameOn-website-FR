e7fa6b83c9f5a8c4d02f1b87d70046957d707fcc
maevanapcontact <maevanap.contact@gmail.com>
2020-12-18 21:23:40 +0100

    [#1] feat: close modal on click on the X button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,35 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/* DOM ELEMENTS
+-------------------------------------
+------------------------------------- */
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll("#close");
 
+
+/* EVENTS
+-------------------------------------
+------------------------------------- */
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+
+
+/* FUNCTIONS
+-------------------------------------
+------------------------------------- */
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
