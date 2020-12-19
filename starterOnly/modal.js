a764e0529ee7cfbfd216b519e02b7149b9384fac
maevanapcontact <maevanap.contact@gmail.com>
2020-12-19 16:24:35 +0100

    [#2] feat: add check for first and last names

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,7 +14,12 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll("#close");
+const formElt = document.querySelectorAll("#form");
 
+// Form elements
+const firstElt = document.getElementById("first");
+const lastElt = document.getElementById("last");
+const quantityElt = document.getElementById("quantity");
 
 /* EVENTS
 -------------------------------------
@@ -24,6 +29,9 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
 closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+
+// Validate form
+formElt.forEach(elt => elt.addEventListener("submit", validate));
 
 
 /* FUNCTIONS
@@ -39,3 +47,16 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Validate form
+function validate(e) {
+  e.preventDefault();
+  const isFirstValid = isLongEnough(firstElt.value.length, 2) ? true : false;
+  console.log(isFirstValid);
+  const isLastValid = isLongEnough(lastElt.value.length, 2) ? true : false;
+  console.log(isLastValid);
+}
+
+// utils: check the length of a value
+function isLongEnough(currentLength, minimumLength) {
+  return currentLength >= minimumLength ? true : false;
+}
